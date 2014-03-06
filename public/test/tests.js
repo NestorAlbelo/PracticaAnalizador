f0108a862e586423fe153f63729e75f3447a5ce0
Alberto Fariña <alu0100695816@ull.edu.es>
2014-03-06 17:12:58 +0000

    Idioma español

--- public/test/tests.js
+++ public/test/tests.js
@@ -1,10 +1,9 @@
 var assert = chai.assert;
 
 suite('Pruebas de Tokens', function() {
-    test('Comprobando comentarios', function() {
+    /*test('Comprobando comentarios', function() {
         $("#INPUT").val = "var a = 2;"
         main();
-        assert.deepEqual($("#OUTPUT").html(),  )
-    });
-    
+        assert.deepEqual($("#OUTPUT").html(), "{\n    \"value\": \"=\",\n    \"arity\": \"binary\",\n    \"first\": {\n        \"value\": \"a\",\n        \"arity\": \"name\"\n    },\n    \"second\": {\n        \"value\": 2,\n        \"arity\": \"literal\"\n    }\n}" )
+    });*/
 });
